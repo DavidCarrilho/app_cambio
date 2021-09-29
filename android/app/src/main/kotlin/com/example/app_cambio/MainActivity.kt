0ddbdbefa3d70963590eddeb896dfb87bb38fd98
DavidCarrilho <luan.david.oliver@gmail.com>
2021-09-29 09:04:09 -0300

    create MaterialApp and MyhomePage

---
+++ android/app/src/main/kotlin/com/example/app_cambio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_cambio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+    hintColor: Colors.green,
+    primaryColor: Colors.white
+  ),
+));
+
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
