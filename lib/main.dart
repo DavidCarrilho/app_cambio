90146ed611d6ace0004a47a2f9af67d0eae93bbc
DavidCarrilho <luan.david.oliver@gmail.com>
2021-09-30 00:28:13 -0300

    create mothods

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,85 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 
-main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-    hintColor: Colors.green,
-    primaryColor: Colors.white
-  ),
-));
+Future<Map> getData() async {
+  String request = 'https://api.hgbrasil.com/finance?format=json&key=84c1fbd6';
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
 
+main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          hintColor: Colors.green,
+          primaryColor: Colors.white,
+        ),
+        home: Home(),
+      ),
+    );
 
-class MyHomePage extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _clearAll() {
+    realController.text = '';
+    dolarController.text = '';
+    euroController.text = '';
+  }
+
+  // Cada um desses métodos converte o valor recebido de String
+  // para double para realizar o cálculo de quanto a moeda vale perante
+  // as demais, e, atribui este valor ao atributo text dos controllers
+
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ElevatedButton.icon(
+      icon: Icon(Icons.ac_unit),
+      label: Text('da'),
+      onPressed: () {},
     );
   }
 }
